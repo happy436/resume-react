b7ce318b27ad0423e47377d474dee81d49a287d8
happy436 <lelouch436@gmail.com>
2022-01-18 20:42:24 +0200

    firebase host

--- src/index.js
+++ src/index.js
@@ -7,25 +7,47 @@ import { Footer } from './shared/footer/Footer';
 import { BrowserRouter } from 'react-router-dom';
 import store from "./redux/redux-store"
 
-const baseURL = "/resume-react"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB9FDBajB-cyH4vY5gojOBzjuiVWuDTr4E",
+    authDomain: "resume-react-29acd.firebaseapp.com",
+    projectId: "resume-react-29acd",
+    storageBucket: "resume-react-29acd.appspot.com",
+    messagingSenderId: "219310398984",
+    appId: "1:219310398984:web:b8bb31bf60d8104266bbbd",
+    measurementId: "G-PXL8RMWTLD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const baseURL = ""
 
 let reRender = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <Header 
-                    language={store.getState().languageReducer.activeLanguage} 
-                    baseURL={baseURL} 
+                <Header
+                    language={store.getState().languageReducer.activeLanguage}
+                    baseURL={baseURL}
                     dispatch={store.dispatch}
                     theme={store.getState().themeReducer.activeTheme}
                 />
-                <App 
-                    state={store.getState().languageReducer} 
+                <App
+                    state={store.getState().languageReducer}
                     baseURL={baseURL}
                 />
-                <Footer 
-                    language={store.getState().languageReducer.activeLanguage} 
-                    baseURL={baseURL} 
+                <Footer
+                    language={store.getState().languageReducer.activeLanguage}
+                    baseURL={baseURL}
                 />
             </BrowserRouter>
         </React.StrictMode>,

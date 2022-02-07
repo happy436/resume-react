a692a6e14e78d01ca5a2e574d486125ee7f92238
happy436 <lelouch436@gmail.com>
2022-02-07 20:54:47 +0200

    refactor

--- src/shared/header/components/changeTheme/ChangeTheme.jsx
+++ src/shared/header/components/changeTheme/ChangeTheme.jsx
@@ -9,9 +9,9 @@ export const ChangeTheme = (props) => {
 
     useEffect(() => {
         if(window.matchMedia('(prefers-color-scheme: dark').matches){
-            document.getElementsByTagName('body')[0].classList.add("dark-theme")
+            document.body.classList.toggle("dark-theme")
         } else if(window.matchMedia('(prefers-color-scheme: light').matches){
-            document.getElementsByTagName('body')[0].classList.remove("dark-theme")
+            document.body.classList.toggle("dark-theme")
             if(theme === 0){
                 changeTheme()
             }
@@ -19,15 +19,14 @@ export const ChangeTheme = (props) => {
     },[])
 
     function changeTheme(){
+        document.body.classList.toggle("dark-theme")
         return dispatch(themeChangeActionCreator())
     }
 
     function changeBtn(){
         if(theme === 0){
-            document.getElementsByTagName('body')[0].classList.add("dark-theme")
             return <UilSun className={s.change_theme} onClick={changeTheme}/>
-        } else if(theme === 1){
-            document.getElementsByTagName('body')[0].classList.remove("dark-theme")
+        } else{
             return <UilMoon className={s.change_theme} onClick={changeTheme}/>
         }
     }

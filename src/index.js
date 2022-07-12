5ed58b0f626ac94ddfd4e2fd70408bd0973905c5
happy436 <lelouch436@gmail.com>
2022-07-12 18:33:35 +0300

    firebase ignore

--- src/index.js
+++ src/index.js
@@ -8,28 +8,6 @@ import { BrowserRouter } from "react-router-dom"
 import store from "./redux/redux-store"
 import Context from "./context"
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app"
-import { getAnalytics } from "firebase/analytics"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyB9FDBajB-cyH4vY5gojOBzjuiVWuDTr4E",
-    authDomain: "resume-react-29acd.firebaseapp.com",
-    projectId: "resume-react-29acd",
-    storageBucket: "resume-react-29acd.appspot.com",
-    messagingSenderId: "219310398984",
-    appId: "1:219310398984:web:b8bb31bf60d8104266bbbd",
-    measurementId: "G-PXL8RMWTLD"
-}
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app) // eslint-disable-line no-unused-vars
-
 const baseURL = ""
 
 function Main() {
@@ -44,20 +22,9 @@ function Main() {
                 }}
             >
                 <BrowserRouter>
-                    <Header
-                    /* language={store.getState().languageReducer.activeLanguage}
-                        baseURL={baseURL}
-                        dispatch={store.dispatch}
-                        theme={store.getState().themeReducer.activeTheme} */
-                    />
-                    <App
-                    /* state={store.getState().languageReducer}
-                        baseURL={baseURL} */
-                    />
-                    <Footer
-                    /* language={store.getState().languageReducer.activeLanguage}
-                        baseURL={baseURL} */
-                    />
+                    <Header />
+                    <App />
+                    <Footer />
                 </BrowserRouter>
             </Context.Provider>
         </React.StrictMode>
